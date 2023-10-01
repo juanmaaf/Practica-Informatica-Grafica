33f6f40d18190b16e676396e16055ebdefc79af3
Juan Manuel Aneas Franco <juanmanuelaf@correo.ugr.es>
2023-10-01 13:41:47 +0200

    Fin CasaX y arreglo Camara

--- src/camara.cpp
+++ src/camara.cpp
@@ -181,7 +181,7 @@ void CamaraOrbitalSimple::actualizarMatrices()
       near      = 0.05,
       far       = near+1000.0 ;
 
-   matriz_proye = perspective( fovy_grad, 1.0f/ratio_vp, near, far ); //MAT_Perspectiva( fovy_grad, ratio_vp, near, far ); // CUA: ratio_vp es y/x, pero esta función esperael 'aspect', que parece ser x/y
+   matriz_proye = perspective( radians(fovy_grad), 1.0f/ratio_vp, near, far ); //MAT_Perspectiva( fovy_grad, ratio_vp, near, far ); // CUA: ratio_vp es y/x, pero esta función esperael 'aspect', que parece ser x/y
    matrices_actualizadas = true ;
 }
 // -----------------------------------------------------------------------------
